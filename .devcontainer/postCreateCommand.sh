9b9e5e3b3d51298b13d3b8e84c0b5ed42950c434
bjones1 <bjones@ece.msstate.edu>
2023-03-06 13:54:36 -0600

    wip

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -7,7 +7,7 @@ echo "Updating pip..."
 python3 -m pip install --user --upgrade pip
 
 echo "Installing Python tools..."
-python3 -m pip install --user CodeChat-Server runestone
+python3 -m pip install --user CodeChat-Server runestone bookserver
 
 echo "Installing iVerilog..."
 sudo apt update
@@ -16,4 +16,6 @@ sudo apt install -y verilog
 echo "Installing Digital System Design textbook..."
 git clone https://github.com/bjones1/digital_systems_design.git
 cd digital_systems_design
-python3 waf configure
+# A Runestone build must be run before waf configure will work, even though the waf build Runestone runs will fail.
+runestone build
+python3 waf configure --no-arm --no-pic24
