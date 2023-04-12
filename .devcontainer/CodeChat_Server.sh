ffb2da9698be26945cd970a7733f8b0074168a31
bjones1 <bjones@ece.msstate.edu>
2023-04-12 15:50:46 -0500

    Docs: Add in CodeChat Server script docs.

--- .devcontainer/CodeChat_Server.sh
+++ .devcontainer/CodeChat_Server.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/env bash
 
-# For some reason, there's often junk on the opened ports. Start the server to let it read this junk, so the next invocation will be clean.
+# <h1><code>CodeChat_Server.sh</code> - Start the CodeChat Server</h1>
+# <p>For some reason, there's often junk on the opened ports. Start the server
+#     to let it read this junk, so the next invocation will be clean. Kill that,
+#     then restart a "clean" server for actual use.</p>
 CodeChat_Server serve &
 sleep 1
 kill %1
