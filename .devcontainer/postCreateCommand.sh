e0d0ae2de409b47858075b0742c4ab97a7639bcc
bjones1 <bjones@ece.msstate.edu>
2023-04-12 15:47:47 -0500

    Clean: Remove image, since we're using a Dockerfile instead. Docs: Improve.

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,13 +1,17 @@
 #!/usr/bin/env bash
 
-# Create a new PreTeXt project.
+# <h1><code>postCreateCommand.sh</code> - Perform installs after the docker
+#     container build process</h1>
+# <p>These commands are run as the owner of the repo, instead of root.</p>
+# <p>Create a new PreTeXt project.</p>
 pretext new -d .
 
-# For the Digital System Design book...
+# <p>For the Digital System Design book...</p>
 #echo "Installing Digital System Design textbook..."
 #apt install iverilog
 #git clone https://github.com/bjones1/digital_systems_design.git
 #cd digital_systems_design
-# A Runestone build must be run before waf configure will work, even though the waf build Runestone runs will fail.
+# <p>A Runestone build must be run before waf configure will work, even though
+#     the waf build Runestone runs will fail.</p>
 #runestone build
 #python3 waf configure --no-arm --no-pic24
